f1fba3da55bed6a8da303c0a5849c4399b6778e8
Johnson Alala <jonesalala95@gmail.com>
2025-05-28 02:40:25 +0300

    Database Table

--- lib/department.py
+++ lib/department.py
@@ -1,12 +1,59 @@
 from __init__ import CURSOR, CONN
 
-
 class Department:
-
     def __init__(self, name, location, id=None):
         self.id = id
         self.name = name
         self.location = location
 
-    def __repr__(self):
-        return f"<Department {self.id}: {self.name}, {self.location}>"
+    @classmethod
+    def create_table(cls):
+        sql = """
+            CREATE TABLE IF NOT EXISTS departments (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                location TEXT
+            )
+        """
+        CURSOR.execute(sql)
+        CONN.commit()
+
+    @classmethod
+    def drop_table(cls):
+        CURSOR.execute("DROP TABLE IF EXISTS departments")
+        CONN.commit()
+
+    def save(self):
+        if self.id is None:
+            sql = """
+                INSERT INTO departments (name, location)
+                VALUES (?, ?)
+            """
+            CURSOR.execute(sql, (self.name, self.location))
+            CONN.commit()
+            self.id = CURSOR.lastrowid
+        else:
+            self.update()
+
+    @classmethod
+    def create(cls, name, location):
+        department = cls(name, location)
+        department.save()
+        return department
+
+    def update(self):
+        sql = """
+            UPDATE departments
+            SET name = ?, location = ?
+            WHERE id = ?
+        """
+        CURSOR.execute(sql, (self.name, self.location, self.id))
+        CONN.commit()
+
+    def delete(self):
+        sql = """
+            DELETE FROM departments
+            WHERE id = ?
+        """
+        CURSOR.execute(sql, (self.id,))
+        CONN.commit()
